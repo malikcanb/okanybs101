ec3b5f92d34a50a0af931de171b7992487ac3ea9
malikcanb <32534894+malikcanb@users.noreply.github.com>
2018-05-09 21:30:58 +0300

    Update finalcevap.java

--- finalcevap.java
+++ finalcevap.java
@@ -6,7 +6,7 @@ public class finalcevap {
 
 	public static void main(String[] args) {
 		Scanner tara = new Scanner(System.in);
-		System.out.println("Kaç kişi girilecek?");
+		System.out.println("KaÃ§ kiÅŸi girilecek?");
 		int gsayi= tara.nextInt();
 		int yas [] = new int[gsayi];
 		int maas[] = new int [gsayi];
@@ -20,28 +20,68 @@ public class finalcevap {
 			meslek[i]= tara.next();
 			System.out.println("Cinsiyet giriniz");
 			cinsiyet[i]= tara.next();
-			System.out.println("Yaş giriniz");
+			System.out.println("YaÅŸ giriniz");
 			yas[i]= tara.nextInt();
-			System.out.println("Maaş giriniz");
+			System.out.println("MaaÅŸ giriniz");
 			maas[i]= tara.nextInt();
 		}
-		System.out.println("Ne istiyorsun? 1- listeleme 2- isim ile arama 3 -yaşların ortu 4-öğretmenlerin bilgileri 5-erkeklerin maaş ortu ");
+		for(;;) {
+		System.out.println("Ne istiyorsun? 1- listeleme 2- isim ile arama 3 -yaÅŸlarÄ±n ortu 4-Ã¶ÄŸretmenlerin bilgileri 5-erkeklerin maaÅŸ ortu ");
 		int semizotu = tara.nextInt();
 		switch(semizotu) {
 		case 1:
 			listeleme(isim,yas,meslek,maas,cinsiyet,gsayi);
 			break;
 		case 2:
-			System.out.println("Kimi arıyorsunuz?");
+			System.out.println("Kimi arÄ±yorsunuz?");
 			String arananad= tara.next();
 			arama(isim,yas,meslek,maas,cinsiyet,gsayi,arananad);
 			break;
 		case 3:
+			yasort(yas,gsayi);
 			break;
 		case 4:
+			ogretmenbilgileri(isim,yas,meslek,maas,cinsiyet,gsayi);
+			
 			break;
 		case 5:
+			erkekmaasort(isim,yas,meslek,maas,cinsiyet,gsayi);
+			
 			break;
+		}
+		}
+	}
+
+	private static void erkekmaasort(String[] isim, int[] yas, String[] meslek, int[] maas, String[] cinsiyet,
+			int gsayi) {
+		int erkeksayisi=0, erkekmaastop=0;
+		for(int i=0;i<gsayi;i++) {
+			if(cinsiyet[i]=="erkek") {
+				erkekmaastop=erkekmaastop+maas[i];
+				erkeksayisi++;
+				System.out.println("erkeklerin maaÅŸ ortu="+erkekmaastop/erkeksayisi);
+			}
+		}
+	}
+
+	private static void ogretmenbilgileri(String[] isim, int[] yas, String[] meslek, int[] maas, String[] cinsiyet,
+			int gsayi) {
+		for(int i=0;i<gsayi;i++) {
+			if(meslek[i]=="Ã¶ÄŸretmen") {
+				System.out.println("Ä°sim="+isim[i]);
+				System.out.println("YaÅŸ="+yas[i]);
+				System.out.println("MesleÄŸiniz="+meslek[i]);
+				System.out.println("Cinsiyetiniz="+cinsiyet[i]);
+				System.out.println("maaÅŸÄ±nÄ±z="+maas[i]);
+			}
+		}
+	}
+
+	private static void yasort(int[] yas, int gsayi) {
+		int toplamyas=0;
+		for(int i=0; i<gsayi;i++) {
+			toplamyas=toplamyas+yas[i];
+			System.out.println("yaÅŸ ortalamasÄ±="+toplamyas/gsayi);
 		}
 	}
 
@@ -49,11 +89,11 @@ public class finalcevap {
 			String arananad) {
 			for(int i=0;i<gsayi;i++) {
 				if(arananad.equalsIgnoreCase(isim[i])) {
-					System.out.println("İsim="+isim[i]);
-					System.out.println("Yaş="+yas[i]);
-					System.out.println("Mesleğiniz="+meslek[i]);
+					System.out.println("Ä°sim="+isim[i]);
+					System.out.println("YaÅŸ="+yas[i]);
+					System.out.println("MesleÄŸiniz="+meslek[i]);
 					System.out.println("Cinsiyetiniz="+cinsiyet[i]);
-					System.out.println("maaşınız="+maas[i]);
+					System.out.println("maaÅŸÄ±nÄ±z="+maas[i]);
 				}
 			}
 	}
@@ -61,11 +101,11 @@ public class finalcevap {
 	private static void listeleme(String[] isim, int[] yas, String[] meslek, int[] maas, String[] cinsiyet, int gsayi) {
 		for(int i=0;i<gsayi;i++)
 		{
-		System.out.println("İsim="+isim[i]);
-		System.out.println("Yaş="+yas[i]);
-		System.out.println("Mesleğiniz="+meslek[i]);
+		System.out.println("Ä°sim="+isim[i]);
+		System.out.println("YaÅŸ="+yas[i]);
+		System.out.println("MesleÄŸiniz="+meslek[i]);
 		System.out.println("Cinsiyetiniz="+cinsiyet[i]);
-		System.out.println("maaşınız="+maas[i]);
+		System.out.println("maaÅŸÄ±nÄ±z="+maas[i]);
 		}
 	}
 
